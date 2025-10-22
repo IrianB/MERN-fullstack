3db2c86a7f933f398faee54212d7d07b17f669c7
Mirabed <irianharyllbecera@gmail.com>
2025-10-22 23:45:23 +0800

    controllers part 2

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -13,7 +13,7 @@ const getWorkout = async (req, res) => {
     const {id} = req.params
 
     if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+        return res.status(404).json({error: 'No such id'})
     }
 
     const workout = await Workout.findById(id)
@@ -39,11 +39,45 @@ const createWorkout = async (req, res) => {
 }
 
 // DELETE a workout
+const deleteWorkout = async (req, res) => {
+    const {id} = req.params
+    
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such id'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if(!workout){
+        return res.status(400).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 // UPDATE a workout
+const updateWorkout = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'no such id'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!workout) {
+        return res.status(400).json({error: 'no such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const {
-   createWorkout,
-   getWorkout,
-   getWorkouts, 
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
 
 // GET all workouts
-router.get('/' , getWorkouts)
+router.get('/', getWorkouts)
 
 // GET a single workout
 router.get('/:id', getWorkout)
@@ -17,13 +19,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // Delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssge:'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssge:'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
